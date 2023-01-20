435f886b194c0e13859aae60951cc35f2435507b
rakhighodke <rakhighodke24@gmail.com>
2023-01-20 23:51:49 +0530

    Added Liskedlistdemo

---
+++ src/com/velocity/arraylist/LinkedListDemo.java
@@ -0,0 +1,13 @@
+package com.velocity.arraylist;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList<String> list=new LinkedList<String>();
+		list.add("pooja");
+		list.add("rekha");
+		System.out.println(list);
+	}
+
+}
